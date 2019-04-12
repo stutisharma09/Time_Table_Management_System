5fa1841be41c621201fe710a5f42e9334446f3cf
Manpreet <itsmanpreetsinghjuneja@gmail.com>
2019-04-12 21:01:10 +0000

    Fixed a bug

--- TimeTable/management/models.py
+++ TimeTable/management/models.py
@@ -5,4 +5,4 @@ class Department(models.Model):
     Name =  models.CharField(max_length=100)
     Department_Code = models.CharField(max_length=3, primary_key = True)
     def __str__(self):
-        return self.name
+        return self.Name
