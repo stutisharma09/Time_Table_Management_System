8a7ae9be523af338e34356a0390cf637ffc4c2d0
Manpreet <itsmanpreetsinghjuneja@gmail.com>
2019-04-12 16:51:45 +0000

    Added Department Model

--- TimeTable/management/models.py
+++ TimeTable/management/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Department(models.Model):
+    Name =  models.CharField(max_length=100)
+    Department_Code = models.CharField(max_length=3, primary_key = True)
+    def __str__(self):
+        return self.name
