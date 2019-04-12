247658eca99dc677bc2b63d87c4bf85f97e3888c
Manpreet <itsmanpreetsinghjuneja@gmail.com>
2019-04-12 11:40:50 +0000

    Changed to MySQL

--- TimeTable/TimeTable/settings.py
+++ TimeTable/TimeTable/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'TimeTable.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'TimeTable_db',
+	'USER' : 'Manpreet',
+	'PASSWORD' : 'manpreet'
     }
 }
 
