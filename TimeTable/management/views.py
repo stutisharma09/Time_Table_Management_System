21b6b3bbfa4feabb8c4e9d391ab109b2e570eef4
Manpreet <itsmanpreetsinghjuneja@gmail.com>
2019-04-12 12:02:46 +0000

    Added first view

--- TimeTable/management/views.py
+++ TimeTable/management/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+
+def index(request):
+    return HttpResponse("Welcome to the Time TimeTable Management Website!")
 # Create your views here.
