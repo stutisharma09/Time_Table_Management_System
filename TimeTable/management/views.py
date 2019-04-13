3264376eea00617ec57d84ee0ed5a13c2d22733d
Manpreet <itsmanpreetsinghjuneja@gmail.com>
2019-04-13 00:33:24 +0000

    Add Student View

--- TimeTable/management/views.py
+++ TimeTable/management/views.py
@@ -3,5 +3,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Welcome to the Time TimeTable Management Website!")
+    return HttpResponse("<h1>Welcome to the Time Table Management Website!</h1>")
+
+
 # Create your views here.
